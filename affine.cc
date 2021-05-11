e0f7e025a983da8e8a2a465bb3685c9fd7c5742b
Jonathan Guiang <jkguiang@gmail.com>
2021-05-11 15:56:31 -0700

    fixed money leak

--- affine.cc
+++ affine.cc
@@ -1,4 +1,5 @@
 #include <stdio.h>   // printf
+#include <math.h>    // tanh
 #include <omp.h>     // OpenMP pragmas
 #include <algorithm> // shuffle
 #include <cstring>   // memcpy
@@ -19,7 +20,7 @@ double xorshift64(XORShiftState* state) {
     x ^= x >> 27; // c
     state->a = x;
     auto u64val = x*0x2545F4914F6CDD1D;
-    double dval = (double)u64val/(double)std::numeric_limits<uint64_t>::max();
+    double dval = double(u64val)/double(std::numeric_limits<uint64_t>::max());
     return dval;
 }
 
@@ -65,27 +66,34 @@ int main(int arc, char** argv)
     const size_t N_ACTORS = getAffineEnvVar<int>("AFFINE_N_ACTORS");
     const size_t N_EPOCHS = getAffineEnvVar<int>("AFFINE_N_EPOCHS");
     const int EPOCHS_PER_SAVE = getAffineEnvVar<int>("AFFINE_N_EPOCHS_PER_SAVE");
-    const size_t N_LORENZ_POINTS = getAffineEnvVar<int>("AFFINE_N_LORENZ_POINTS");
     const double INIT_WEALTH = getAffineEnvVar<double>("AFFINE_INIT_WEALTH");
     const double TRANSACT_SIZE = getAffineEnvVar<double>("AFFINE_TRANSACT_SIZE");
     const double CHI = getAffineEnvVar<double>("AFFINE_CHI");
     const double ZETA = getAffineEnvVar<double>("AFFINE_ZETA");
     const double KAPPA = getAffineEnvVar<double>("AFFINE_KAPPA");
-    printf("%d, %f, %f, %f\n", N_ACTORS, CHI, ZETA, KAPPA);
     // Initialize other Affine-related constants
     const double total_wealth = N_ACTORS*INIT_WEALTH;
     const double loan = (KAPPA/(1. - KAPPA))*INIT_WEALTH;
     // Initialize actors
-    double actor_wealth[N_ACTORS];
+    double bank[N_ACTORS];
     unsigned int actors[N_ACTORS];
     for (unsigned int actor = 0; actor < N_ACTORS; actor++)
     {
-        actor_wealth[actor] = INIT_WEALTH;
+        bank[actor] = INIT_WEALTH;
         actors[actor] = actor;
     }
-    // Initialize Lorenz Curve
-    double cumulative_wealth[N_LORENZ_POINTS];
-    double cumulative_actors[N_LORENZ_POINTS];
+    // Initialize Lorenz curve variables
+    const size_t n_lorenz_points = 1000;
+    double lorenz_fracs[n_lorenz_points];
+    for (unsigned int i = 0; i < n_lorenz_points-5; i++)
+    {
+        lorenz_fracs[i] = (1./N_ACTORS)*i/(n_lorenz_points-5);
+    }
+    lorenz_fracs[n_lorenz_points-1] = 0.9;
+    lorenz_fracs[n_lorenz_points-2] = 0.8;
+    lorenz_fracs[n_lorenz_points-3] = 0.5;
+    lorenz_fracs[n_lorenz_points-4] = 0.25;
+    lorenz_fracs[n_lorenz_points-5] = 0.1;
     // Run the simulation
     unsigned seed = getTime();
     XORShiftState* xorshift_state = new XORShiftState(seed);
@@ -97,53 +105,55 @@ int main(int arc, char** argv)
             std::default_random_engine(seed)
         );
         // Run transactions
-        for (unsigned int i = 0; i < N_ACTORS-1; i++)
+        for (unsigned int i = 0; i < N_ACTORS-1; i += 2)
         {
             // Get actor and partner
-            unsigned int actor = actors[i];
-            unsigned int partner = actors[i+1];
+            unsigned int a = actors[i];
+            unsigned int p = actors[i+1];
             // Distribute loans
-            actor_wealth[actor] += loan;
-            actor_wealth[partner] += loan;
-            // Retrieve actor and partner wealth
-            double a_wealth = actor_wealth[actor];
-            double p_wealth = actor_wealth[partner];
+            bank[a] += loan;
+            bank[p] += loan;
             // Determine transaction value
             double transact;
-            if (a_wealth < p_wealth) { transact = TRANSACT_SIZE*a_wealth; }
-            else { transact = TRANSACT_SIZE*p_wealth; }
+            if (bank[a] < bank[p]) { transact = TRANSACT_SIZE*bank[a]; }
+            else { transact = TRANSACT_SIZE*bank[p]; }
             // Determine outcome of biased coin toss
-            double prob_minus1 = 0.5*(1 - ZETA*(a_wealth - p_wealth)/(INIT_WEALTH));
+            double prob_minus1 = 0.5*(1 - ZETA*(bank[a] - bank[p])/(INIT_WEALTH));
             bool actor_won = (xorshift64(xorshift_state) > prob_minus1);
             // Apply transaction to actor and partner wealth
-            actor_wealth[actor] += transact*pow(-1, !actor_won);
-            actor_wealth[partner] += transact*pow(-1, actor_won);
+            bank[a] += transact*pow(-1, !actor_won);
+            bank[p] += transact*pow(-1, actor_won);
             // Redistribute wealth
-            actor_wealth[actor] += (INIT_WEALTH - actor_wealth[actor])*CHI;
-            actor_wealth[partner] += (INIT_WEALTH - actor_wealth[partner])*CHI;
+            bank[a] += (INIT_WEALTH+loan - bank[a])*CHI;
+            bank[p] += (INIT_WEALTH+loan - bank[p])*CHI;
             // Collect loans
-            actor_wealth[actor] -= loan;
-            actor_wealth[partner] -= loan;
+            bank[a] -= loan;
+            bank[p] -= loan;
         }
         // Only compute Lorenz curve at checkpoints or on final epoch
-        if (epoch % EPOCHS_PER_SAVE != 0 && epoch != N_EPOCHS - 1) { continue; }
+        if (epoch % EPOCHS_PER_SAVE != 0 && epoch != N_EPOCHS-1) { continue; }
+        // Print CSV headers
+        if (epoch == 0) { printf("epoch,cumulative_wealth,cumulative_actors\n"); }
         // Compute Lorenz curve
-        for (unsigned int i = 0; i < N_LORENZ_POINTS; i++)
+        for (unsigned int i = 0; i < n_lorenz_points; i++)
         {
-            double wealth_frac = pow(2., i)/pow(2., N_LORENZ_POINTS - 1)*total_wealth;
-            cumulative_wealth[i] = 0.;
-            cumulative_actors[i] = 0.;
+            double wealth_frac = lorenz_fracs[i]*total_wealth;
+            double cumulative_wealth = 0.; // frac of total wealth held by actors w/ wealth < wealth frac
+            double cumulative_actors = 0.; // frac of total actors w/ wealth < wealth frac
             for (unsigned int actor = 0; actor < N_ACTORS; actor++)
             {
-                if (actor_wealth[actor] < wealth_frac) 
+                if (bank[actor] < wealth_frac) 
                 {
-                    cumulative_wealth[i] += actor_wealth[actor];
-                    cumulative_actors[i] += 1.0;
+                    cumulative_wealth += bank[actor];
+                    cumulative_actors += 1.0;
                 }
             }
-            cumulative_wealth[i] /= total_wealth;
-            cumulative_actors[i] /= (double)N_ACTORS;
+            cumulative_wealth /= total_wealth;
+            cumulative_actors /= double(N_ACTORS);
+            // Print CSV row
+            printf("%d,%f,%f\n", epoch, cumulative_wealth, cumulative_actors);
         }
+        printf("%d,%f,%f\n", epoch, 1.0, 1.0);
     }
     return 0;
 }
