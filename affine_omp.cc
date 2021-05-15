fc7668ee1a841d83d57724e2298c52d46b38b0b6
Jonathan Guiang <jkguiang@gmail.com>
2021-05-14 17:11:38 -0700

    added fixed omp (but seg faults)

--- affine_omp.cc
+++ affine_omp.cc
@@ -4,6 +4,7 @@
 #include <random>    // rand, default_random_engine
 #include <limits>    // numeric_limits
 #include <chrono>    // utc timestamps
+#include <cstring>   // memcpy
 #include <string>    // probably self-evident
 
 #include "utils.icc"
@@ -11,6 +12,7 @@
 int main(int arc, char** argv)
 {
     // Retrieve Affine parameters from environment vars
+    const size_t N_THREADS = Utils::getAffineEnvVar<int>("OMP_NUM_THREADS");
     const size_t N_ACTORS = Utils::getAffineEnvVar<int>("AFFINE_N_ACTORS");
     const size_t N_EPOCHS = Utils::getAffineEnvVar<int>("AFFINE_N_EPOCHS");
     const int EPOCHS_PER_SAVE = Utils::getAffineEnvVar<int>("AFFINE_N_EPOCHS_PER_SAVE");
@@ -24,11 +26,13 @@ int main(int arc, char** argv)
     const double total_wealth = N_ACTORS*INIT_WEALTH;
     const double loan = (KAPPA/(1. - KAPPA))*INIT_WEALTH;
     // Initialize Affine variables
-    double bank[N_ACTORS];
+    double bank_orig[N_ACTORS];
+    double bank_next[N_ACTORS];
     unsigned int actors[N_ACTORS];
     for (unsigned int actor = 0; actor < N_ACTORS; actor++)
     {
-        bank[actor] = INIT_WEALTH;
+        bank_orig[actor] = INIT_WEALTH;
+        bank_next[actor] = INIT_WEALTH;
         actors[actor] = actor;
     }
     double transact;
@@ -45,85 +49,97 @@ int main(int arc, char** argv)
     lorenz_fracs[N_LORENZ_POINTS-3] = 0.5;
     lorenz_fracs[N_LORENZ_POINTS-4] = 0.25;
     lorenz_fracs[N_LORENZ_POINTS-5] = 0.1;
-    double cumulative_wealth[N_LORENZ_POINTS] = {0.};
-    double cumulative_actors[N_LORENZ_POINTS] = {0.};
+    double cumulative_wealth = 0.;
+    double cumulative_actors = 0.;
     // Initialize iterating indices
-    unsigned int i;
+    unsigned int i; // generic index
     unsigned int a; // actor
     unsigned int p; // partner
+    unsigned int t; // thread
     // Run the simulation
-    unsigned seed = Utils::getTime();
-    Utils::XORShiftState* xorshift_state;
+    int seed = Utils::getTime();
+    Utils::XORShiftState* xorstates_orig[N_THREADS];
+    Utils::XORShiftState* xorstates_next[N_THREADS];
+    for (i = 0; i < N_THREADS; i++)
+    {
+        xorstates_orig[N_THREADS] = new Utils::XORShiftState(seed*(i+1));
+    }
     for (unsigned int epoch = 0; epoch < N_EPOCHS; epoch++)
     {
         // Shuffle partners
         shuffle(
             &actors[0], &actors[N_ACTORS], 
-            std::default_random_engine(seed*epoch)
+            std::default_random_engine(seed)
         );
+        // Compute the next timestep
         #pragma omp parallel \
-        shared(INIT_WEALTH, TRANSACT_SIZE, CHI, ZETA, bank, loan, actors) \
-        private(i, a, p, transact, prob_minus1, actor_won, xorshift_state)
+        shared(INIT_WEALTH, TRANSACT_SIZE, CHI, ZETA, bank_orig, loan, actors, xorstates_orig) \
+        private(i, a, p, t, transact, prob_minus1, actor_won)
         {
-            xorshift_state = new Utils::XORShiftState(seed*omp_get_thread_num());
+            t = omp_get_thread_num();
+            xorstates_next[t] = xorstates_orig[t];
             // Run transactions
-            #pragma omp for
+            #pragma omp for lastprivate(bank_next, xorstates_next)
             for (i = 0; i < N_ACTORS-1; i += 2)
             {
                 // Get actor and partner
                 a = actors[i];
                 p = actors[i+1];
                 // Distribute loans
-                bank[a] += loan;
-                bank[p] += loan;
+                bank_next[a] = bank_orig[a] + loan;
+                bank_next[p] = bank_orig[p] + loan;
                 // Determine transaction value
-                if (bank[a] < bank[p]) { transact = TRANSACT_SIZE*bank[a]; }
-                else { transact = TRANSACT_SIZE*bank[p]; }
+                if (bank_next[a] < bank_next[p]) 
+                {
+                    transact = TRANSACT_SIZE*bank_next[a]; 
+                }
+                else 
+                { 
+                    transact = TRANSACT_SIZE*bank_next[p]; 
+                }
                 // Determine outcome of biased coin toss
-                prob_minus1 = 0.5*(1 - ZETA*(bank[a] - bank[p])/(INIT_WEALTH));
-                actor_won = (Utils::xorshift64(xorshift_state) > prob_minus1);
+                prob_minus1 = 0.5*(1-ZETA*(bank_next[a]-bank_next[p])/(INIT_WEALTH));
+                actor_won = (Utils::xorshift64(xorstates_next[t]) > prob_minus1);
                 // Apply transaction to actor and partner wealth
-                bank[a] += transact*pow(-1, !actor_won);
-                bank[p] += transact*pow(-1, actor_won);
+                bank_next[a] += transact*pow(-1, !actor_won);
+                bank_next[p] += transact*pow(-1, actor_won);
                 // Redistribute wealth
-                bank[a] += (INIT_WEALTH+loan - bank[a])*CHI;
-                bank[p] += (INIT_WEALTH+loan - bank[p])*CHI;
+                bank_next[a] += (INIT_WEALTH+loan - bank_next[a])*CHI;
+                bank_next[p] += (INIT_WEALTH+loan - bank_next[p])*CHI;
                 // Collect loans
-                bank[a] -= loan;
-                bank[p] -= loan;
+                bank_next[a] -= loan;
+                bank_next[p] -= loan;
             }
         }
+        // Set original arrays equal to the next timestep computed above
+        std::memcpy(bank_orig, bank_next, sizeof(double)*N_ACTORS);
+        std::memcpy(xorstates_orig, xorstates_next, sizeof(xorstates_orig));
         // Only compute Lorenz curve at checkpoints or on final epoch
         if (epoch % EPOCHS_PER_SAVE != 0 && epoch != N_EPOCHS-1) { continue; }
         // Print CSV headers
         if (epoch == 0) { printf("epoch,cumulative_wealth,cumulative_actors\n"); }
         // Compute Lorenz curve
         #pragma omp parallel \
-        shared(N_ACTORS, total_wealth, bank, lorenz_fracs, cumulative_wealth, cumulative_actors) \
-        private(i, a)
+        shared(N_ACTORS, total_wealth, bank_next, lorenz_fracs) \
+        private(i, a) \
+        firstprivate(cumulative_wealth, cumulative_actors)
         {
-            #pragma omp for collapse(2)
+            #pragma omp for
             for (i = 0; i < N_LORENZ_POINTS; i++)
             {
                 for (a = 0; a < N_ACTORS; a++)
                 {
-                    if (bank[a] < lorenz_fracs[i]*total_wealth) 
+                    if (bank_next[a] < lorenz_fracs[i]*total_wealth) 
                     {
-                        cumulative_wealth[i] += bank[a];
-                        cumulative_actors[i] += 1.0;
+                        cumulative_wealth += bank_next[a];
+                        cumulative_actors += 1.;
                     }
                 }
+                cumulative_wealth /= total_wealth;
+                cumulative_actors /= double(N_ACTORS);
+                // Print CSV row
+                printf("%d,%f,%f\n", epoch, cumulative_wealth, cumulative_actors);
             }
-        }
-        for (i = 0; i < N_LORENZ_POINTS; i++)
-        {
-            cumulative_wealth[i] /= total_wealth;
-            cumulative_actors[i] /= double(N_ACTORS);
-            // Print CSV row
-            printf("%d,%f,%f\n", epoch, cumulative_wealth[i], cumulative_actors[i]);
-            // Reset
-            cumulative_wealth[i] = 0.;
-            cumulative_actors[i] = 0.;
         }
     }
     return 0;
